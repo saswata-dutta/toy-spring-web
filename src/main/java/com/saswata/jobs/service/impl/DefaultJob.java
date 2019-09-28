47187643ecb0330d4425f58408e8cbb10bcfdcf7
saswata-dutta <saswat.dutta@gmail.com>
2019-09-28 13:51:59 +0530

    impls of methods

--- src/main/java/com/saswata/jobs/service/impl/DefaultJob.java
+++ src/main/java/com/saswata/jobs/service/impl/DefaultJob.java
@@ -5,7 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -28,21 +31,45 @@ public class DefaultJob implements Job {
 
   @Override
   public boolean add(int id, int value) {
-    return false;
+    if (idIndex.containsKey(id)) return false;
+
+    idIndex.put(id, value);
+
+    if (valueIndex.containsKey(value)) {
+      Set<Integer> oldIds = valueIndex.get(value);
+      oldIds.add(id);
+      valueIndex.put(value, oldIds);
+    } else {
+      Set<Integer> idSet = new HashSet<>(Collections.singletonList(id));
+      valueIndex.put(value, idSet);
+    }
+    return true;
   }
 
   @Override
   public boolean remove(int id) {
-    return false;
+    if (!idIndex.containsKey(id)) return false;
+
+    int value = idIndex.get(id);
+    idIndex.remove(id);
+    Set<Integer> oldIds = valueIndex.get(value);
+    oldIds.remove(id);
+    valueIndex.put(value, oldIds);
+
+    return true;
   }
 
   @Override
   public List<Integer> getAll() {
-    return null;
+    List<Integer> jobs = new ArrayList<>();
+    valueIndex.values().forEach(jobs::addAll);
+    return jobs;
   }
 
   @Override
   public List<Integer> getAllAfter(int startValue) {
-    return null;
+    List<Integer> jobs = new ArrayList<>();
+    valueIndex.tailMap(startValue).values().forEach(jobs::addAll);
+    return jobs;
   }
 }
